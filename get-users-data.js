4b02f290cc26de3aafe3821e80478599c9d60ead
egor_ <egor_spaik05@mail.ru>
2023-12-03 22:00:02 +0300

    6-7

--- get-users-data.js
+++ get-users-data.js
@@ -1,27 +1,47 @@
 document.addEventListener('DOMContentLoaded', function () {
     const swiperWrapper = document.getElementById('swiper-wrapper');
     const loader = document.getElementById('loader');
+    const errorContainer = document.getElementById('error-container');
 
-    function fetchData() {
-        loader.style.display = 'block'; 
+    let requestCount = 0;
 
-        fetch("https://jsonplaceholder.typicode.com/users")
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`Network response was not ok: ${response.status}`);
-                }
-                return response.json();
-            })
-            .then(data => {
-                renderUsers(data);
+    function fetchData(userIds) {
+        loader.style.display = 'block';
+        errorContainer.style.display = 'none';
+
+        const filterParam = requestCount % 2 === 0 ? 'id_gte=100' : 'id_lte=200';
+
+        const promises = userIds.map(userId =>
+            fetch(`https://jsonplaceholder.typicode.com/users/${userId}?${filterParam}`)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error(`Network response was not ok: ${response.status}`);
+                    }
+                    return response.json();
+                }).catch(error => {
+                    console.error(`⚠ Something went wrong: ${error.message}`);
+                    showError(error.message);
+                }) 
+        );
+
+        Promise.all(promises)
+            .then(usersData => {
+                renderUsers(usersData);
                 initSwiper();
-                loader.style.display = 'none'; 
+                loader.style.display = 'none';
             })
             .catch(error => {
                 console.error(`⚠ Something went wrong: ${error.message}`);
-                loader.style.display = 'none'; 
+                showError(error.message);
+                loader.style.display = 'none';
             });
+
+        requestCount++;
     }
+
+    const randomUserIds = Array.from({ length: 5 }, () => Math.floor(Math.random() * 10) + 1);
+
+    fetchData(randomUserIds);
 
     function renderUsers(users) {
         users.forEach(user => {
@@ -30,7 +50,9 @@ document.addEventListener('DOMContentLoaded', function () {
             userCard.innerHTML = `
                 <img src="https://via.placeholder.com/150" alt="${user.name}">
                 <h3>${user.name}</h3>
-                <p>${user.email}</p>
+                <p>Email: ${user.email}</p>
+                <p>Phone: ${user.phone}</p>
+                <p>Website: ${user.website}</p>
             `;
             swiperWrapper.appendChild(userCard);
         });
@@ -48,5 +70,11 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
-    fetchData();
+    function showError(errorMessage) {
+        if (errorContainer) {
+            errorContainer.textContent = errorMessage;
+            errorContainer.style.display = 'block';
+        }
+    }
 });
+
